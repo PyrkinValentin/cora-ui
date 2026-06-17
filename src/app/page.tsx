5e0997fe87ffda62afc064afcdfe31b9fd39eb55
Валентин Пыркин <elemental-hero@yandex.by>
2026-06-17 23:45:24 +0300

    feat(ui): add Skeleton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import { Slider } from "@/ui/components"
+import {} from "@/ui/components"
 
 export default function Home() {
 	return (
 		<div className="w-full sm:w-lg mt-75 h-90 flex flex-col gap-2 mx-auto">
-			<Slider.Root orientation="horizontal" defaultValue={25}>
-				<div className="flex items-center justify-between gap-2">
-					<Slider.Label>Volume</Slider.Label>
-					<Slider.Value/>
-				</div>
 
-				<Slider.Control>
-					<Slider.Track>
-						<Slider.Indicator/>
-						<Slider.Thumb/>
-					</Slider.Track>
-				</Slider.Control>
-
-			</Slider.Root>
 		</div>
 	)
 }
---
+++ src/ui/components/skeleton/skeleton.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import type { SkeletonProps } from "./skeleton.props"
+
+import { toClassNames } from "../../utils"
+
+import { Render } from "../render"
+
+export const Skeleton = (props: SkeletonProps) => {
+	const {
+		className,
+		...restProps
+	} = props
+
+	return (
+		<Render
+			{...restProps}
+			defaultTagName="div"
+			aria-hidden="true"
+			className={toClassNames("skeleton", className)}
+		/>
+	)
+}
+
+Skeleton.displayName = "Skeleton"
