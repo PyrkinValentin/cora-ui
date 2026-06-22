76a837e2fce212767f4806abca462890c84363b0
Валентин Пыркин <elemental-hero@yandex.by>
2026-06-22 12:43:13 +0300

    style(dialog): refactor popup animations and fix nested dialog layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,56 @@
-import { Avatar, Button, Item } from "@/ui/components"
+import { Avatar, Button, Dialog, Item } from "@/ui/components"
 
 export default function Home() {
 	return (
 		<div className="p-4 w-full sm:w-lg h-dvh mx-auto flex flex-col gap-4 items-center justify-center">
+			<Dialog.Root>
+				<Dialog.Trigger>Open dialog</Dialog.Trigger>
 
+				<Dialog.Portal>
+					<Dialog.Backdrop/>
+
+					<Dialog.Popup>
+						<Dialog.Close nativeClose/>
+
+						<Dialog.Title>What is Lorem Ipsum?</Dialog.Title>
+
+						<Dialog.Description>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+							Lorem
+							Ipsum
+							has been the industry's standard dummy
+						</Dialog.Description>
+
+						<Dialog.Actions>
+							<Button className="w-full">Continue</Button>
+
+							<Dialog.Root>
+								<Dialog.Trigger>Open nested dialog</Dialog.Trigger>
+
+								<Dialog.Portal>
+									<Dialog.Backdrop/>
+
+									<Dialog.Popup>
+										<Dialog.Close nativeClose/>
+
+										<Dialog.Title>What is Lorem Ipsum?</Dialog.Title>
+										<Dialog.Description>
+											Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+											Lorem
+											Ipsum
+											has been the industry's standard dummy
+										</Dialog.Description>
+
+										<Dialog.Actions>
+											<Button className="w-full">Continue</Button>
+										</Dialog.Actions>
+									</Dialog.Popup>
+								</Dialog.Portal>
+							</Dialog.Root>
+						</Dialog.Actions>
+					</Dialog.Popup>
+				</Dialog.Portal>
+			</Dialog.Root>
 		</div>
 	)
 }
