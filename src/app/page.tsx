24151e77b6f7802951fd521e8d736bee861ae34a
Валентин Пыркин <elemental-hero@yandex.by>
2026-06-22 13:22:35 +0300

    style(Badge): refactor styles Badge.Indicator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,26 @@
-import { Avatar, Button, Dialog, Item } from "@/ui/components"
+"use client"
+
+import { Badge, Button } from "@/ui/components"
+import { useState } from "react";
 
 export default function Home() {
+	const [invisible, setInVisible] = useState(false)
+
 	return (
 		<div className="p-4 w-full sm:w-lg h-dvh mx-auto flex flex-col gap-4 items-center justify-center">
-			<Dialog.Root>
-				<Dialog.Trigger>Open dialog</Dialog.Trigger>
+			<Badge.Root>
+				<Button onClick={() => setInVisible(!invisible)}>Button</Button>
 
-				<Dialog.Portal>
-					<Dialog.Backdrop/>
-
-					<Dialog.Popup>
-						<Dialog.Close nativeClose/>
-
-						<Dialog.Title>What is Lorem Ipsum?</Dialog.Title>
-
-						<Dialog.Description>
-							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-							Lorem
-							Ipsum
-							has been the industry's standard dummy
-						</Dialog.Description>
-
-						<Dialog.Actions>
-							<Button className="w-full">Continue</Button>
-
-							<Dialog.Root>
-								<Dialog.Trigger>Open nested dialog</Dialog.Trigger>
-
-								<Dialog.Portal>
-									<Dialog.Backdrop/>
-
-									<Dialog.Popup>
-										<Dialog.Close nativeClose/>
-
-										<Dialog.Title>What is Lorem Ipsum?</Dialog.Title>
-										<Dialog.Description>
-											Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-											Lorem
-											Ipsum
-											has been the industry's standard dummy
-										</Dialog.Description>
-
-										<Dialog.Actions>
-											<Button className="w-full">Continue</Button>
-										</Dialog.Actions>
-									</Dialog.Popup>
-								</Dialog.Portal>
-							</Dialog.Root>
-						</Dialog.Actions>
-					</Dialog.Popup>
-				</Dialog.Portal>
-			</Dialog.Root>
+				<Badge.Indicator
+					invisible={invisible}
+					size="sm"
+					status="warning"
+					side="top"
+					align="end"
+				>
+					3
+				</Badge.Indicator>
+			</Badge.Root>
 		</div>
 	)
 }
