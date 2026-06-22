d014448ae97ddcd62326bb3064a23f24e6402335
Валентин Пыркин <elemental-hero@yandex.by>
2026-06-22 18:04:51 +0300

    style(Checkbox, Radio): refactor styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
-import { OTPField } from "@/ui/components"
+import { Checkbox, Radio, RadioGroup, Switch } from "@/ui/components"
 
 export default function Home() {
 	return (
 		<div className="p-4 w-full sm:w-lg h-dvh mx-auto flex flex-col gap-4 items-center justify-center">
-			<OTPField.Root length={6}>
-				<OTPField.Input/>
-				<OTPField.Input/>
-				<OTPField.Input/>
+			<Switch.Root>
+				<Switch.Thumb/>
+			</Switch.Root>
 
-				<OTPField.Separator/>
+			<label className="flex items-center gap-2 select-none">
+				<Checkbox.Root>
+					<Checkbox.Indicator/>
+				</Checkbox.Root>
+				Option
+			</label>
 
-				<OTPField.Input/>
-				<OTPField.Input/>
-				<OTPField.Input/>
-			</OTPField.Root>
+			<RadioGroup>
+				<Radio.Root value={0}>
+					<Radio.Indicator/>
+				</Radio.Root>
+
+				<Radio.Root value={1}>
+					<Radio.Indicator/>
+				</Radio.Root>
+
+				<Radio.Root value={2}>
+					<Radio.Indicator/>
+				</Radio.Root>
+			</RadioGroup>
 		</div>
 	)
 }
