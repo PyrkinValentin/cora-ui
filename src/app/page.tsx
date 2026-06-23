2eb49a753ac4d68147369ad188a1ac2c28b5607e
Валентин Пыркин <elemental-hero@yandex.by>
2026-06-23 15:56:32 +0300

    style(all): refactor styles all components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
-import { Checkbox, Radio, RadioGroup, Switch } from "@/ui/components"
+import {} from "@/ui/components"
 
 export default function Home() {
 	return (
-		<div className="p-4 w-full sm:w-lg h-dvh mx-auto flex flex-col gap-4 items-center justify-center">
-			<Switch.Root>
-				<Switch.Thumb/>
-			</Switch.Root>
+		<div className="p-4 w-full sm:w-2xl h-dvh mx-auto flex flex-col gap-2 items-center justify-center">
 
-			<label className="flex items-center gap-2 select-none">
-				<Checkbox.Root>
-					<Checkbox.Indicator/>
-				</Checkbox.Root>
-				Option
-			</label>
-
-			<RadioGroup>
-				<Radio.Root value={0}>
-					<Radio.Indicator/>
-				</Radio.Root>
-
-				<Radio.Root value={1}>
-					<Radio.Indicator/>
-				</Radio.Root>
-
-				<Radio.Root value={2}>
-					<Radio.Indicator/>
-				</Radio.Root>
-			</RadioGroup>
 		</div>
 	)
 }
